ed44566f7a160925a5648c153e872ce832b14f30
rahulsgit <rahulrr1220@gmail.com>
2025-08-13 21:29:52 +0530

    num change2

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ export default function App() {
     const text = encodeURIComponent(
       `Name: ${nameRef.current.value}\nEmail: ${emailRef.current.value}\nMessage: ${msgRef.current.value}`
     );
-    window.open(`https://wa.me/919500976049?text=${text}`, "_blank");
+    window.open(`https://wa.me/+919789269677?text=${text}`, "_blank");
   };
 
   return (
